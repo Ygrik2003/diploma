10187870ac3ae73152b5f67ff6f80bb32798f622
Igor Stepanov <i_stepanov2@lesta.group>
2025-02-03 19:30:31 +0300

    Refactoring

---
+++ wrapper.py
@@ -0,0 +1,15 @@
+import numpy as np
+import tensorflow as tf
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices("GPU")))
+
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+
+
+def load_model():
+    pass
+
+
+def save_model(model: tf.keras.Model):
+    pass
